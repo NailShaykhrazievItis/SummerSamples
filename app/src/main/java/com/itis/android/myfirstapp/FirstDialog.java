4317e44cde86600c08954f690771f1e3f3bc2767
Nail <shaykhrazievnail@gmail.com>
2018-07-10 12:40:57 +0300

    add listener in dialog

--- app/src/main/java/com/itis/android/myfirstapp/FirstDialog.java
+++ app/src/main/java/com/itis/android/myfirstapp/FirstDialog.java
@@ -1,6 +1,7 @@
 package com.itis.android.myfirstapp;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -8,34 +9,53 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 /**
  * Created by Nail Shaykhraziev on 10.07.2018.
  */
 public class FirstDialog extends DialogFragment {
 
-    private EditText editText;
+    private EditText email;
+    private EditText login;
+    private OnFieldsFill listener;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof SecondActivity) {
+            listener = (OnFieldsFill) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFieldsFill");
+        }
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         super.onCreateDialog(savedInstanceState);
-        View veiw = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_first, null);
-        editText = veiw.findViewById(R.id.et_email);
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_first, null);
+        email = view.findViewById(R.id.et_email);
+        login = view.findViewById(R.id.et_login);
         AlertDialog.Builder adb = new AlertDialog.Builder(getActivity());
         adb.setTitle("Title")
-                .setView(veiw)
-                .setPositiveButton("Ok", (dialog, which) -> {
-                    Toast.makeText(getActivity(), editText.getText().toString(), Toast.LENGTH_SHORT).show();
+                .setView(view)
+                .setPositiveButton("Apply", (dialog, which) -> {
+                   listener.onOkClick(email.getText().toString(), login.getText().toString());
                 })
-                .setNegativeButton("No", (dialog, which) -> {
+                .setNegativeButton("No, Thanks", (dialog, which) -> {
                     dismiss();
                 });
-        return adb.show();
+        return adb.create();
     }
 
-    private void click() {
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
 
+    interface OnFieldsFill {
+        void onOkClick(String email, String login);
     }
 }
--- app/src/main/java/com/itis/android/myfirstapp/SecondActivity.java
+++ app/src/main/java/com/itis/android/myfirstapp/SecondActivity.java
@@ -5,17 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements FirstDialog.OnFieldsFill {
+
+    private TextView tvLogin;
+    private TextView tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        tvLogin = findViewById(R.id.tv_login);
+        tvEmail = findViewById(R.id.tv_email);
         String text = getIntent().getStringExtra("key");
         Toast.makeText(this, text, Toast.LENGTH_LONG).show();
-        findViewById(R.id.btn_show).setOnClickListener(v->{
+        findViewById(R.id.btn_show).setOnClickListener(v -> {
             FirstDialog dialog = new FirstDialog();
             dialog.show(getSupportFragmentManager(), "dialog");
         });
@@ -26,5 +32,11 @@ public class SecondActivity extends AppCompatActivity {
         intent.putExtra("result", "result from second screen");
         setResult(Activity.RESULT_OK, intent);
         finish();
+    }
+
+    @Override
+    public void onOkClick(String email, String login) {
+        tvLogin.setText(getString(R.string.login, login));
+        tvEmail.setText(getString(R.string.email, email));
     }
 }
